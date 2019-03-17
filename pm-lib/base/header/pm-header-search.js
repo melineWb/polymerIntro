b0a42630392313f81fe76ea4fb010892f7ba56e4
melineWb <meline.pe081@gmail.com>
2019-03-17 22:52:40 +0200

    fix css

--- pm-lib/base/header/pm-header-search.js
+++ pm-lib/base/header/pm-header-search.js
@@ -9,20 +9,20 @@ class PmHeaderSearch extends PmComponent {
       ${super.template}
 
       <style>
-        .btn-outline-pm {
+        .pm-form .btn-outline-pm {
           color: var(--pm-primary-color);
           border-color: var(--pm-primary-color);
         }
 
-        .btn-outline-pm:hover {
+        .pm-form .btn-outline-pm:hover {
           color: var(--pm-hover-color);
           border-color: var(--pm-hover-color);
           background: transparent;
         }
       </style>
-      
+
       <template is="dom-if" if="{{enableSearching}}">
-        <form class="form-inline my-2 my-lg-0" on-submit="search">
+        <form class="form-inline my-2 my-lg-0 pm-form" on-submit="search">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
           <button class="btn btn-outline-pm my-2 my-sm-0" type="submit">[[button]]</button>
         </form>
